50705f7f63c65e7eed95b9a1d4615d96f24e0b48
Janarbek <janarbek.bekmuyrzauulu.alt@gmail.com>
2024-04-16 12:53:20 +0600

    janarbek

---
+++ src/pages/homePage/homePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function homePage() {
+  return (
+    <div>
+      <h1 className='text-3xl mt-9'>Home Page Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur a itaque dicta eos ipsa maiores architecto quidem laborum quisquam! Cum impedit suscipit itaque, dolorum doloremque iusto tenetur repudiandae obcaecati voluptates. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, quisquam quo quod hic labore minus facere sequi assumenda aliquid possimus fugiat in veritatis culpa sed voluptates magnam reiciendis, saepe repudiandae?</h1>
+    </div>
+  )
+}
+
+export default homePage
