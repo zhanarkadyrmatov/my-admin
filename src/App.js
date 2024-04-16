8019e2c612fd89e71c5ef261c60b46f52b6e185a
ernis2005 <nhtfxnhtfx.9@gmail.com>
2024-04-16 12:55:28 +0600

    fix

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Playground from "./components/Playground/Playground";
 import Header from "./components/Header/Header";
+import HomePage from "./pages/homePage/homePage";
 
 function App() {
   const [collapsed, setCollapsed] = useState(false);
@@ -26,16 +27,10 @@ function App() {
           darkMode={darkMode}
         />
         <div className="overflow-y-auto h-screen px-4 py-[35px] lg:py-[55px] xl:px-6 xl:py-[60px] bg-[#f5f5f5] dark:bg-[#212130]">
-          <Routes>
-            <Route path="/" element={<div>
-              <h1 className='text-3xl mt-9'>Home Page Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur a itaque dicta eos ipsa maiores architecto quidem laborum quisquam! Cum impedit suscipit itaque, dolorum doloremque iusto tenetur repudiandae obcaecati voluptates. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, quisquam quo quod hic labore minus facere sequi assumenda aliquid possimus fugiat in veritatis culpa sed voluptates magnam reiciendis, saepe repudiandae?
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur a itaque dicta eos ipsa maiores architecto quidem laborum quisquam! Cum impedit suscipit itaque, dolorum doloremque iusto tenetur repudiandae obcaecati voluptates. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, quisquam quo quod hic labore minus facere sequi assumenda aliquid possimus fugiat in veritatis culpa sed voluptates magnam reiciendis, saepe repudiandae?
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur a itaque dicta eos ipsa maiores architecto quidem laborum quisquam! Cum impedit suscipit itaque, dolorum doloremque iusto tenetur repudiandae obcaecati voluptates. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, quisquam quo quod hic labore minus facere sequi assumenda aliquid possimus fugiat in veritatis culpa sed voluptates magnam reiciendis, saepe repudiandae?
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur a itaque dicta eos ipsa maiores architecto quidem laborum quisquam! Cum impedit suscipit itaque, dolorum doloremque iusto tenetur repudiandae obcaecati voluptates. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, quisquam quo quod hic labore minus facere sequi assumenda aliquid possimus fugiat in veritatis culpa sed voluptates magnam reiciendis, saepe repudiandae?
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur a itaque dicta eos ipsa maiores architecto quidem laborum quisquam! Cum impedit suscipit itaque, dolorum doloremque iusto tenetur repudiandae obcaecati voluptates. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, quisquam quo quod hic labore minus facere sequi assumenda aliquid possimus fugiat in veritatis culpa sed voluptates magnam reiciendis, saepe repudiandae?
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur a itaque dicta eos ipsa maiores architecto quidem laborum quisquam! Cum impedit suscipit itaque, dolorum doloremque iusto tenetur repudiandae obcaecati voluptates. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, quisquam quo quod hic labore minus facere sequi assumenda aliquid possimus fugiat in veritatis culpa sed voluptates magnam reiciendis, saepe repudiandae? Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur a itaque dicta eos ipsa maiores architecto quidem laborum quisquam! Cum impedit suscipit itaque, dolorum doloremque iusto tenetur repudiandae obcaecati voluptates. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, quisquam quo quod hic labore minus facere sequi assumenda aliquid possimus fugiat in veritatis culpa sed voluptates magnam reiciendis, saepe repudiandae?
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur a itaque dicta eos ipsa maiores architecto quidem laborum quisquam! Cum impedit suscipit itaque, dolorum doloremque iusto tenetur repudiandae obcaecati voluptates. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, quisquam quo quod hic labore minus facere sequi assumenda aliquid possimus fugiat in veritatis culpa sed voluptates magnam reiciendis, saepe repudiandae?</h1>
-            </div>} />
+
+        <Routes>
+        <Route path="/" element={<HomePage />} /> 
+            
           </Routes>
         </div>
       </main>
--- src/pages/homePage/homePage.jsx
+++ src/pages/homePage/homePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function homePage() {
+function HomePage() {
   return (
     <div>
       <h1 className='text-3xl mt-9'>Home Page Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur a itaque dicta eos ipsa maiores architecto quidem laborum quisquam! Cum impedit suscipit itaque, dolorum doloremque iusto tenetur repudiandae obcaecati voluptates. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, quisquam quo quod hic labore minus facere sequi assumenda aliquid possimus fugiat in veritatis culpa sed voluptates magnam reiciendis, saepe repudiandae?</h1>
@@ -8,4 +8,4 @@ function homePage() {
   )
 }
 
-export default homePage
+export default HomePage
