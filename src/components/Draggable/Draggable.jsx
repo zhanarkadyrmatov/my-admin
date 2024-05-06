be4a3b650e85e18914c33497058be39b1a3b00da
aziret006 <aziretaalybaev2006@gmail.com>
2024-05-06 16:39:41 +0600

    fix

--- src/components/Draggable/Draggable.jsx
+++ src/components/Draggable/Draggable.jsx
@@ -3,11 +3,7 @@ import s from "./draggable.module.scss";
 import { GoPlus } from "react-icons/go";
 
 export default function Draggable() {
-<<<<<<<<< Temporary merge branch 1
-=========
 
-
->>>>>>>>> Temporary merge branch 2
   const redireact = () => {
     window.location.href = "/calendary/book";
   };
@@ -21,7 +17,6 @@ export default function Draggable() {
             </h4>
             <select name="" id="">
               <option value="">Today</option>
->>>>>>>>> Temporary merge branch 2
               <option value="">Tomorrow</option>
               <option value="">This Week</option>
             </select>
@@ -33,8 +28,6 @@ export default function Draggable() {
             <td>День</td>
             <td>Время</td>
           </tr>
-<<<<<<<<< Temporary merge branch 1
-
           {[1, 2, 3, 4, 5, 6, 7, 8, , 3, 45, 5, 6, 7, 8, 9, 0, 0, 9].map(
             (res, i) => (
               <tr key={i}>
@@ -44,20 +37,6 @@ export default function Draggable() {
               </tr>
             )
           )}
-        </table>
-
-        <div className={s.draggable_button}>
-          <button onClick={() => redireact()}>
-            <GoPlus size={20} />
-            Create New
-=========
-          {[1, 2, 3, 4, 5, 6, 7, 8, , 3, 45, 5, 6, 7, 8, 9, 0, 0, 9].map((res, i) => (
-            <tr key={i}>
-              <td style={{ color: '#000' }}>John Mathew Kayne</td>
-              <td>06.08.2020</td>
-              <td>17:30 - 19:00</td>
-            </tr>
-          ))}
         </table>
         <div className={''}>
           <button className="flex justify-center items-center gap-2 w-full py-[10px] px-4 rounded-xl  border bg-[#f93a0b] hover:bg-[#a43418] duration-300" onClick={() => redireact()} >
