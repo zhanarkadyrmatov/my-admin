0253911a35dc4f918cfa0806774b4073c57d8dc7
aziret006 <aziretaalybaev2006@gmail.com>
2024-05-20 14:02:01 +0600

    fix

--- src/components/Cards/arbitrCards/Acards.jsx
+++ src/components/Cards/arbitrCards/Acards.jsx
@@ -4,14 +4,14 @@ import arbitr from "../../../img/arbitr.svg";
 export default function Acards() {
   return (
     <>
-      <div className="flex items-center justify-between w-full h-[70px]">
+      <div className="sm:flex items-center justify-between w-full h-[70px] grid">
         <div className="flex justify-between w-[275px]">
           <img src={arbitr} alt="" />
           <div className="grid items-center h-[2px] py-[7px]">
             <h5 className="text-[15px] font-medium leading-tight text-left">
               ataialanov
             </h5>
-            <li className="flex gap-[2px] items-center list-none text-xs font-medium leading-tight text-left">
+            <li className="flex gap-x-[2px] items-center list-none text-xs font-medium leading-tight text-left">
               El Clasico{" "}
               <span className="w-[3px] h-[3px] bg-[#222222] opacity-70 rounded-full"></span>
               <span className="text-xs font-medium leading-tight text-left opacity-70">
@@ -30,4 +30,4 @@ export default function Acards() {
       </div>
     </>
   );
-};
+}
