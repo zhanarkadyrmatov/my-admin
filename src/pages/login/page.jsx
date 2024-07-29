601d738484500e5358129154bd45613486fb9151
Bekmyrza uulu Janarbek <99035622+zhanarkadyrmatov@users.noreply.github.com>
2024-07-29 12:21:52 +0600

    janar

--- src/pages/login/page.jsx
+++ src/pages/login/page.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
-import "./page.scss";
 import cm from "classnames";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUser } from "../../store/slice/user.slice";
-import { IoEyeSharp } from "react-icons/io5";
-import { FaEyeSlash } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa";
+
+
 function Login() {
   const [email, setEmail] = useState({ value: "", error: "" });
   const [showPassword, setShowPassword] = useState(false);
@@ -31,61 +32,56 @@ function Login() {
       navigate("/");
     }
   }, [status]);
+
   return (
     <div
-      className="Login dark:bg-[#17171E] dark:text-[#000]"
-      style={{ marginTop: "30px" }}
-    >
-      <div className="Login__container dark:bg-[#212130] dark:text-[#fff]">
-        <p className="Login__p--hint">
-          Войти вы можете только зарегистрированным <br /> пользователем
+      className="w-full h-[100vh] flex justify-center items-center bg-[#f5f5f5]">
+      <div className="flex flex-col gap-[24px] justify-center items-center md:max-w-[475px] rounded-[10px] h-[auto] w-[90%] md:w-full bg-[#ffff] md:px-[30px] px-[18px] py-[40px] md:py-[60px] shadow-md">
+        <p className="md:text-[20px] text-[16px] font-[500] text-center ">
+          Войти вы можете только зарегистрированным пользователем
         </p>
-        {error?.detail && <p className="Login__p--error">{error.detail}</p>}
-        <label>
-          <p>Email</p>
+        {error?.detail && <p className="font-[500] text-[#f93a0b] text-[14px]">{error.detail}</p>}
+        <label className="flex flex-col gap-[5px] w-full">
+          <p className="md:text-[16px] text-[14px] font-normal ">Email</p>
           <input
             onChange={(e) => setEmail((i) => ({ value: e.target.value }))}
             value={email.value}
             type="email"
             placeholder="Email"
             className={cm(
-              "Login__input",
-              "dark:bg-[#212130]  dark:text-[#fff]",
-              { "Login__input--error": email.error }
+              "bg-[#e0dddd] rounded-[5px] md:p-[10px] p-[8px] placeholder:text-[#6c6c6c] outline-none focus-within:border-[green] focus-within:border-[2px] placeholder:text-[14px]",
+              { "border-[#f93a0b] border-[2px]": email.error }
             )}
           />
-          {email.error && <p className="Login__p">{email.error}</p>}
+          {email.error && <p className="font-[500] text-[#f93a0b] text-[14px]">{email.error}</p>}
         </label>
-        <label className="relative">
-          <p>Password</p>
-          <input
-            onChange={(e) => setPassword((i) => ({ value: e.target.value }))}
-            value={password.value}
-            placeholder="Password"
-            type={showPassword ? "text" : "password"}
-            onClick={(e) => setPassword(e.target.value)}
-            className={cm(
-              "Login__input",
-              "dark:bg-[#212130]  dark:text-[#fff]",
-              { "Login__input--error": password.error }
-            )}
-          />
-          <span
-            className="absolute right-[5px] top-[75%] transform -translate-y-1/2 cursor-pointer w-6 h-6"
-            onClick={togglePasswordVisibility}
-          >
-            {showPassword ? <IoEyeSharp /> : <FaEyeSlash />}
-          </span>
-          {password.error && <p className="Login__p">{password.error}</p>}
+        <label className="flex flex-col gap-[5px] w-full">
+          <p className="md:text-[16px] text-[14px] font-normal ">Password</p>
+          <div 
+          className={cm(
+            "relative bg-[#e0dddd] rounded-[5px] w-full md:p-[10px] p-[8px] focus-within:border-[green] focus-within:border-[2px]",
+            { "border-[#f93a0b] border-[2px]": password.error }
+          )}>
+            <input
+              onChange={(e) => setPassword((i) => ({ value: e.target.value }))}
+              value={password.value}
+              placeholder="Password"
+              type={showPassword ? "text" : "password"}
+              onClick={(e) => setPassword(e.target.value)}
+              className={"placeholder:text-[#6c6c6c] outline-none bg-transparent placeholder:text-[14px]"}
+            />
+            <span
+              className="absolute right-[10px] top-[50%] translate-y-[-50%] cursor-pointer"
+              onClick={togglePasswordVisibility}
+            >
+              {showPassword ? <FaEye className="w-[18px] h-[18px]" /> : <FaEyeSlash className="w-[18px] h-[18px]" />}
+            </span>
+          </div>
+          {password.error && <p className="font-[500] text-[#f93a0b] text-[14px]">{password.error}</p>}
         </label>
-        <button type="submit" onClick={handleSubmit}>
-          Login
+        <button className={`w-full bg-[#f93a0b] text-[#fff] duration-300 rounded-[5px] p-[10px] ${ email?.value?.length > 0 && password?.value?.length > 0 ? "opacity-100 hover:bg-[#c4300b]" : 'opacity-50'}`} type="submit" onClick={handleSubmit}>
+          Войти
         </button>
-        <div className="register_context">
-          <h4>
-            Don't have an account? <span>Sign up</span>
-          </h4>
-        </div>
       </div>
     </div>
   );
